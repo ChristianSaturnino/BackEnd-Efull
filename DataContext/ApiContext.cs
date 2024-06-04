983e2d4ade962f692731282020a44c1411de47e7
Christian Saturnino <christian.saturnino95@hotmail.com>
2024-06-03 21:43:39 -0300

    Add

---
+++ DataContext/ApiContext.cs
@@ -0,0 +1,18 @@
+﻿using ApiProva.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProva.DataContext
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ApiProvaDB;ConnectRetryCount=0");
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Central> UsuariosCentral { get; set; }
+
+    }
+}
